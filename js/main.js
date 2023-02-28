ecfa2b2daeef0d8f6586c64aac8d288f5da37cdc
dalimkumar604 <dalimkumar212582@gmail.com>
2023-02-28 22:25:07 +0600

    modal

--- js/main.js
+++ js/main.js
@@ -21,8 +21,27 @@ hamburgerButton.addEventListener("click", showSidebar);
 closeButton.addEventListener("click", hideSidebar);
 
 // progress bar
-
 const progress = document.querySelector(".progress-done");
-
 progress.style.width = progress.getAttribute("data-done") + "%";
 progress.style.opacity = 1;
+
+// connect wallet modal
+
+const connectWallet = document.querySelector("#connectWallet");
+const connectWalletModal = document.querySelector("#connectWalletModal");
+const overlay = document.querySelector(".overlay");
+const modalClose = document.querySelector(".modalClose");
+
+const metamaskConnect = document.querySelector(".metamaskConnect");
+const walletConnect = document.querySelector(".walletConnect");
+
+const showConnectWalletModal = () => {
+  connectWalletModal.style.display = "block";
+  overlay.style.display = "block";
+};
+const hideConnectWalletModal = () => {
+  connectWalletModal.style.display = "none";
+  overlay.style.display = "none";
+};
+connectWallet.addEventListener("click", showConnectWalletModal);
+modalClose.addEventListener("click", hideConnectWalletModal);
