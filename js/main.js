9ae50ff7eb23980567e775aee3b4510b9651f0d5
dalimkumar604 <dalimkumar212582@gmail.com>
2023-03-01 14:04:53 +0600

    slider

--- js/main.js
+++ js/main.js
@@ -110,3 +110,75 @@ purchaseWithEthModalClose.addEventListener("click", () => {
   purchaseWithEthModal.style.display = "none";
   overlay.style.display = "none";
 });
+
+// slider
+
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+let isDragging = false;
+let startPos = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
+
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector("img");
+  slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+  // disable image dragging
+  slide.addEventListener("touchstart", touchStart(index));
+  slide.addEventListener("touchend", touchEnd);
+  slide.addEventListener("touchmove", touchMove);
+  slide.addEventListener("mousedown", touchStart(index));
+  slide.addEventListener("mouseup", touchEnd);
+  slide.addEventListener("mousemove", touchMove);
+  slide.addEventListener("mouseleave", touchEnd);
+});
+
+function touchStart(index) {
+  return function (event) {
+    isDragging = true;
+    startPos = getPositionX(event);
+    animationID = requestAnimationFrame(animation);
+    slider.classList.add("grabbing");
+  };
+}
+
+function touchEnd() {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+  const movedBy = currentTranslate - prevTranslate;
+  if (movedBy < -100 && index < slides.length - 1) {
+    index++;
+  }
+  if (movedBy > 100 && index > 0) {
+    index--;
+  }
+  setPositionByIndex();
+  slider.classList.remove("grabbing");
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event);
+    currentTranslate = prevTranslate + currentPosition - startPos;
+  }
+}
+
+function getPositionX(event) {
+  return event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
+}
+
+function animation() {
+  setSliderPosition();
+  if (isDragging) requestAnimationFrame(animation);
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
+function setPositionByIndex() {
+  currentTranslate = index * -window.innerWidth;
+  prevTranslate = currentTranslate;
+  setSliderPosition();
+}
